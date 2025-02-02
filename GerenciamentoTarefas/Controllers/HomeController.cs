3f196abc57016ca84c1595f0103a7702ff53662d
Rafael Arantes <rafaeltwi@hotmail.com>
2025-02-02 17:52:28 -0300

    Ajustes

--- GerenciamentoTarefas/Controllers/HomeController.cs
+++ GerenciamentoTarefas/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ public class HomeController : Controller
         return RedirectToAction("Index", "Tarefa");
     }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
